538ce6504ce6a1d7ac8bab233b7be6e4b4f02f23
Humayra-Amin <humaema4576@gmail.com>
2024-06-01 01:50:17 +0600

    all setup done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Pet Adoption Server Started')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server started on http://localhost: ${port}`)
+  })
